5985268b802afdc9c1d3c617bb7fb7643f8dce7c
Kavishka Imalsha <kavishkaimalsha@gmail.com>
2025-08-10 19:17:39 +0530

    Design HoverTranslateCard and it Carousel with dummy data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,49 @@
+import HomeCarousel from "../HomeCarousel"
+import image1 from "../assest/image1.png"
+import image2 from "../assest/image2.png"
+import image3 from "../assest/image3.png"
 const Home = () => {
+    const cardInfo = [{
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },  {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },  {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },
+    {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },  {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },  {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      },  {
+        imageUrls: [image1, image2, image3],
+        title: "Per Owned Iphone 16 Pro Max",
+        price: 240000,
+        colors: ["black", "white"]
+      }]
     return(<>
-        <h1>This is homw page</h1>
+        <div className="p-8 min-h-screen">
+            <HomeCarousel slides={cardInfo} />
+        </div>
     </>)
 }
 
